b141da73696cf90cd2b3c91ada2842047975c81b
bleezmo <joshuarogers10@gmail.com>
2013-11-05 12:25:46 -0500

    forgot to remove assignment 3

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,7 +17,7 @@ object Application extends Controller {
   	if(assignment == 0) Ok(views.html.assignments());
   	else if(assignment == 1) Ok(views.html.assignment1());
     //else if(assignment == 2) Ok(views.html.assignment2());
-    else if(assignment == 3) Ok(views.html.assignment3());
+    //else if(assignment == 3) Ok(views.html.assignment3());
   	else NotFound;
   }
 
