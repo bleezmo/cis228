f6d9a0b85d39b4440b9ffa6c273e24467f700e82
bleezmo <joshuarogers10@gmail.com>
2013-11-12 16:14:13 -0500

    assignment 2

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -16,7 +16,7 @@ object Application extends Controller {
   def assignments(assignment:Int) = Action {
   	if(assignment == 0) Ok(views.html.assignments());
   	else if(assignment == 1) Ok(views.html.assignment1());
-    //else if(assignment == 2) Ok(views.html.assignment2());
+    else if(assignment == 2) Ok(views.html.assignment2());
     //else if(assignment == 3) Ok(views.html.assignment3());
   	else NotFound;
   }
