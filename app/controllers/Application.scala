c8446620e22b86792c01a1b0c2c1770863a252b2
bleezmo <joshuarogers10@gmail.com>
2013-12-12 13:35:11 -0500

    added grading

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -27,7 +27,7 @@ object Application extends Controller {
   	else if(assignment == 1) Ok(views.html.assignment1());
     else if(assignment == 2) Ok(views.html.assignment2());
     else if(assignment == 3) Ok(views.html.assignment3());
-    //else if(assignment == 4) Ok(views.html.assignment4());
+    else if(assignment == 4) Ok(views.html.assignment4());
     //else if(assignment == 5) Ok(views.html.assignment5());
   	else NotFound;
   }
