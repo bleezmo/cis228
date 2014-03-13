6fa10dd149b3d61e340ea5a970615bdf5aa85cb0
bleezmo <joshuarogers10@gmail.com>
2014-03-13 14:20:00 -0400

    screwed up

--- public/classes/spring2014/3_13_2014/notes.js
+++ public/classes/spring2014/3_13_2014/notes.js
@@ -1,4 +1,5 @@
 var counter = 0;
+//this function executes each time the Click Me! button is clicked
 function myOnClick(){
 	counter++;
 	document.getElementById("clicktracker").
@@ -10,12 +11,17 @@ window.onload = function(){
 		.innerHTML = "0 button clicks";
 }
 var counter2 = 0;
+//this function executes each time the Look Inside button is clicked
 function lookInside(){
 	counter2++;
 	if(counter2 < 10){
+		//create the button element
 		var btn = document.createElement("button");
+		//set the text of the button
 		btn.innerHTML = "Look Inside"
+		//set the onclick attribute to mirror that of the original Look Inside button
 		btn.setAttribute("onclick","lookInside()");
+		//add the button to the document
 		document.body.appendChild(btn);
 	}
 }
